971c390bf076c87e0291673ae183498e2a0ab2d0
KamiliArsyad <arsyad.ik@gmail.com>
2023-05-18 20:16:07 +0800

    basic server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('server.js loaded');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
