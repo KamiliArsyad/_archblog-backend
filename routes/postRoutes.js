3df3deef2fb50396b79ec9382cd8f5249aacca51
KamiliArsyad <arsyad.ik@gmail.com>
2023-05-19 11:03:44 +0800

    modified postRoutes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,6 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const postController = require('../controllers/postController');
+const postController = require("../controllers/postController");
+const { protect } = require("../middlewares/authMiddleware");
+
+router
+  .route("/")
+  .get(postController.getAllPosts)
+  .post(protect, postController.createPost);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,4 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use('/api/posts', require('./routes/postRoutes'));
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
